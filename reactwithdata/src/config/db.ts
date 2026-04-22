f20580dc48a76a504a3d5a637c5df91e882e3564
Lynriston <xuliosofficial@gmail.com>
2026-04-22 14:53:36 +0300

    db.ts and itemsService.ts

--- reactwithdata/src/config/db.ts
+++ reactwithdata/src/config/db.ts
@@ -1,5 +1,12 @@
+import sql from "mssql"
 
-
-export const dbConfig {
-    
+export const dbConfig: sql.config = {
+    user: "paneEndaArvutiNimi",
+    server: "",
+    password: ""
+    //pärast saab juurde kirjutada.
 }
+
+export const poolPromise = new sql.ConnectionPool(dbConfig)
+    .connect()
+    .then(pool => pool);
--- reactwithdata/src/services/itemsService.ts
+++ reactwithdata/src/services/itemsService.ts
@@ -8,5 +8,15 @@ export const getItems = async () => {
 }
 
 export const addItem = async (name: string) => {
-    
+    const pool = await poolPromise;
+    await pool.request()
+        .input("name", sql.NVarChar, name)
+        .query("insert into Items (name) values (@name)");
+}
+
+export const deleteItem = async (id: number) => {
+    const pool = await poolPromise;
+    await pool.request()
+        .input("id", sql.Int, id)
+        .query("delete from Items where id = @id");
 }
